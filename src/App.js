ee89adf2a618080b75d65d741bb315e87018abb6
hadiyajahagirdar <115102788+hadiyajahagirdar@users.noreply.github.com>
2023-04-15 15:15:39 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Lattitude from './components/Lattitude';
+// import log from './components/login';
+function App() {
+  return (
+    <div className="container">
+     <Lattitude/>
+      
+    </div>
+  );
+}
+
+export default App;
+
